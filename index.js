186ed3812f9a1f22216c57e29e67df1b98d157d5
Wendy Walsh <Wendy@roberts-air.lan>
2018-02-10 16:30:57 -0600

    final attempt

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var $shapeInput = document.querySelector("#shape");
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked at bottom of filter liest
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// Set filteredAddresses to addressData initially
+// Set filtereddata to dataset initially
 var filteredData = dataSet;
 
 // renderTable renders the filteredData to the tbody
@@ -21,14 +21,14 @@ function renderTable() {
   //input data
   $tbody.innerHTML = "";
   
-  for (var i = 0; i < filteredData.length; i++) {
-    // Get get the current address object and its fields
+  for (var i = 0; i < 50; i++) {
+    // Get get the current data object and its fields
     var sighting = filteredData[i];
     var fields = Object.keys(sighting);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+      // For every field in the dataset object, create a new cell at set its inner text to be the current value at the current data field
       var field = fields[j];
       var $cell = $row.insertCell(j);
       $cell.innerText = sighting[field];
@@ -36,40 +36,99 @@ function renderTable() {
   }
 }
 
-
+//search buttom click
 function handleSearchButtonClick() {
   // prevent page from refreshing
   event.preventDefault();
   
-  // //input data
+//input data
   var inputDate = $dateInput.value.trim();
   var inputCity = $cityInput.value.trim().toLowerCase();
   var inputState = $stateInput.value.trim().toLowerCase();
   var inputCountry = $countryInput.value.trim().toLowerCase();
   var inputShape = $shapeInput.value.trim().toLowerCase();
 
-  // // filter data // Format the user's search by removing leading and trailing whitespace, lowercase the string
+
+// Set filtereddatato an array of all data whose input matches the filter
+  if (inputDate != ""){
+    filteredData = dataSet.filter(function(sighting) {
+      var filteredDateTime = sighting.datetime;
+      return filteredDateTime === inputDate;
+    });
+  };
+
+  if(inputCity  != ""){
+    filteredData = dataSet.filter(function(sighting){
+      var filteredCity = sighting.city;
+      return filteredCity ===inputCity;
+    });
+  };
+
+  if(inputState != "")
+  {
+    filteredData = dataSet.filter(function(sighting)
+    {
+      var filteredState = sighting.state;
+      return filteredState ===inputState;
+    });
+  };
+  
+  if(inputCountry != "")
+  {
+    filteredData = dataSet.filter(function(sighting)
+    {
+      var filteredCountry = sighting.country;
+      return filteredCountry === inputCountry;
+    });
+  };
+
+  
+    if(inputShape != "")
+    {
+      filteredData = dataSet.filter(function(sighting)
+      {
+        var filteredShape = sighting.shape;
+        return filteredShape ===inputShape;
+      });
+    };
+  
+
+  renderTable();
+}
+
+renderTable();
+
+// Different formatting, but couldn't get it to work.
  
-  var filteredDateTime = sighting.datetime;
-  var filteredCity = sighting.city;
-  var filteredState = sighting.state;
-  var filteredCountry = sighting.country;
-  var filteredShape = sighting.shape;
+// function handleSearchButtonClick() {
+//   // prevent page from refreshing
+//   event.preventDefault();
+  
+//   // //input data
+//   var inputDate = $dateInput.value.trim();
+//   var inputCity = $cityInput.value.trim().toLowerCase();
+//   var inputState = $stateInput.value.trim().toLowerCase();
+//   var inputCountry = $countryInput.value.trim().toLowerCase();
+//   var inputShape = $shapeInput.value.trim().toLowerCase();
+
+// filter data // Format the user's search by removing leading and trailing whitespace, lowercase the string
+ 
+//   var filteredDateTime = sighting.datetime;
+//   var filteredCity = sighting.city;
+//   var filteredState = sighting.state;
+//   var filteredCountry = sighting.country;
+//   var filteredShape = sighting.shape;
 
   
-  filteredData = dataSet.filter(function(sighting) {
-    return filteredDateTime === inputDate;
-    return filteredCity ===inputCity;
-    return filteredState ===inputState;
-    return filteredCountry === inputCountry;
-    return filteredShape ===inputShape;
-  });
-};
+//   filteredData = dataSet.filter(function(sighting) {
+//     return filteredDateTime === inputDate;
+//     return filteredCity ===inputCity;
+//     return filteredState ===inputState;
+//     return filteredCountry === inputCountry;
+//     return filteredShape ===inputShape;
+//   });
+//   renderTable();  
+// };
 
-renderTable();  
+// renderTable();  
   
-
-
-
-
-
